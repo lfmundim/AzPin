81323b5960ed8cd3e5590fae085fbc95167dae5b
Lucas Mundim <lucas.mundim044@gmail.com>
2026-06-11 16:49:34 +0200

    chore: revert tray icon to system default for GNOME compatibility

--- src/ubuntu/AzPin/src/ui/indicator.rs
+++ src/ubuntu/AzPin/src/ui/indicator.rs
@@ -19,7 +19,7 @@ pub struct AzPinTray {
 
 impl Tray for AzPinTray {
     fn icon_name(&self) -> String {
-        "com.kimdim.azpin-tray".into()
+        "weather-overcast".into()
     }
     
     fn title(&self) -> String {
